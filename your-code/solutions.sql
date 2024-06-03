993e3d14f7b89736e693511ece6d3dc303520278
ivanseldas <ivanseldasp@gmail.com>
2024-06-03 23:45:29 +0200

    update

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -35,3 +35,68 @@ FROM
 	film;
 
 -- 5. Determinar la primera y última fecha de alquiler para cada cliente:
+SELECT 
+	customer.customer_id,
+    FIRST_VALUE(rental.rental_date) OVER (PARTITION BY customer.customer_id) AS first_rental_date,
+    LAST_VALUE(rental.rental_date) OVER (PARTITION BY customer.customer_id) AS last_rental_date
+FROM
+	customer
+INNER JOIN
+	rental ON customer.customer_id = rental.customer_id;
+
+-- 6. Calcular el rango de los clientes basado en el número de sus alquileres:
+WITH rental_counts AS (
+	SELECT 
+		customer.customer_id,
+		COUNT(rental.rental_id) AS count
+	FROM
+		customer
+	INNER JOIN
+		rental ON customer.customer_id = rental.customer_id
+	GROUP BY
+		customer.customer_id
+)
+SELECT 
+	customer_id,
+    count,
+    RANK() OVER (ORDER BY count DESC) AS ranking
+FROM
+	rental_counts;
+
+-- 7. Calcular el total acumulado de ingresos por día para la categoría de películas 'Familiar':
+WITH time_table AS(
+	SELECT
+		film.title,
+        rental.rental_id,
+		EXTRACT(DAY FROM payment.payment_date) AS day,
+        EXTRACT(MONTH FROM payment.payment_date) AS month,
+        EXTRACT(YEAR FROM payment.payment_date) AS year,
+        payment.amount
+	FROM
+		payment
+	INNER JOIN
+		rental ON payment.rental_id = rental.rental_id
+	INNER JOIN
+		inventory ON rental.inventory_id = inventory.inventory_id
+	INNER JOIN 
+		film ON film.film_id = inventory.film_id
+	INNER JOIN
+		film_category ON film.film_id = film_category.film_id
+	INNER JOIN
+		category ON film_category.category_id = category.category_id
+	WHERE category.name = 'Family'
+)
+SELECT
+	rental_id,
+	title,
+    amount,
+    day,
+    month,
+    year,
+    SUM(amount) OVER (PARTITION BY day) AS total_accumulated
+FROM
+	time_table
+ORDER BY
+	year,
+    month,
+    day;
